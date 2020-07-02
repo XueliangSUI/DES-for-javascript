f3795f8d207d7c238151ec11ec3feedbe2923ce5
TheOnlyObserver <1158617847@qq.com>
2020-07-02 18:07:34 +0800

    中文解密完成！样式优化中，小bug懒得调

--- js/more.js
+++ js/more.js
@@ -502,3 +502,50 @@ function filterHex(ChineseArr) {
 	}
 	return ChineseHexArr
 }
+
+function getChinese(Arr) {
+	let t = ''
+	let str = ''
+	if (Arr[1] == '0') {
+		return Arr[0]
+	} else if (Arr[6] == '0') {
+		str = '%'+Arr[0]+Arr[1]+'%'+Arr[2]+Arr[3]+'%'+Arr[4]+Arr[5]
+		return utf8to16(unescape(str))
+	}
+	
+}
+
+/**
+ * 来源：网络 博客园 172257861
+ * https://www.cnblogs.com/kool/p/6695667.html
+ */
+function utf8to16(str) {
+    var out, i, len, c;
+    var char2, char3;
+    out = "";
+    len = str.length;
+    i = 0;
+    while(i < len) {
+		 c = str.charCodeAt(i++);
+		 switch(c >> 4)
+		 { 
+		   case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
+			 out += str.charAt(i-1);
+			 break;
+		   case 12: case 13:
+			 char2 = str.charCodeAt(i++);
+			 out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
+			 break;
+		   case 14:
+			 char2 = str.charCodeAt(i++);
+			 char3 = str.charCodeAt(i++);
+			 out += String.fromCharCode(((c & 0x0F) << 12) |
+				((char2 & 0x3F) << 6) |
+				((char3 & 0x3F) << 0));
+			 break;
+		 }
+    }
+
+    return out;
+}
+
